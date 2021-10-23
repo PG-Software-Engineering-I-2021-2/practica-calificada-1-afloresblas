a2b33760a61b01392f9ca253fc786b8671b5f9f5
afloresblas <90117664+afloresblas@users.noreply.github.com>
2021-10-22 22:35:23 -0500

    second commit

---
+++ src/test/java/domain/PruebaTest.java
@@ -0,0 +1,13 @@
+package domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PruebaTest {
+
+    @Test
+    public void main(){
+        Prueba.main(new String[0]);
+        Assert.assertTrue(true);
+    }
+}
